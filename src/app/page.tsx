bf42f770e7c425d6a78452c8c36ce2ac3cd180d1
Niklas Bialk <niklas.bialk@geberit.com>
2024-01-21 16:58:04 +0100

    ✨ create basic landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
-export default function Home() {
+import Image from "next/image";
 
+export default function Home() {
   return (
-    <main>
-      <p>NGO Management Tool</p>
+    <main className='mt-20'>
+      <div className='flex items-center justify-center space-x-8'>
+        <Image
+          className='rounded-lg'
+          src={"/landing.jpg"}
+          width={600}
+          height={600}
+          alt='Landing'
+        />
+        <p className='text-6xl leading-snug'>
+          Vereine verbinden,
+          <br /> Zukunft gestalten
+        </p>
+      </div>
     </main>
   );
 }
