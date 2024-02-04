e759b8d47e7363892f19a043548affccb3307930
Niklas Bialk <niklas.bialk@geberit.com>
2024-02-04 15:09:13 +0100

    ✨ add simple dashboard ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,12 @@
 "use client";
 
-import Image from "next/image";
 import { useUserStore } from "@/store/user-store";
+
+import Dashboard from "@/components/Dashboard";
+import Landing from "@/components/Landing";
 
 export default function Home() {
   const user = useUserStore((state) => state.user);
 
-  return (
-    <main className='mt-20'>
-      {user ? (
-        <p className='text-center text-4xl'>Hey {user.name}!</p>
-      ) : (
-        <p className='text-center text-4xl'>Nicht angemeldet</p>
-      )}
-      <div className='flex items-center justify-center space-x-8'>
-        <Image
-          className='rounded-lg'
-          src={"/landing.jpg"}
-          width={600}
-          height={600}
-          alt='Landing'
-        />
-        <p className='text-6xl leading-snug'>
-          Vereine verbinden,
-          <br /> Zukunft gestalten
-        </p>
-      </div>
-    </main>
-  );
+  return <main className='mt-20'>{user ? <Dashboard /> : <Landing />}</main>;
 }
