3c148832668e72faf99af6e38a1d62358d364b6f
Niklas Bialk <niklas.bialk@geberit.com>
2024-02-09 19:41:15 +0100

    ✨ implent clerk-mariadb connection

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,13 +1,32 @@
-import { currentUser } from "@clerk/nextjs";
+"use client";
 
+import { useEffect } from "react";
+import { useUser } from "@clerk/nextjs";
+
+import { createUserAction } from "@/actions/user.actions";
 import Dashboard from "@/components/dashboard/Dashboard";
 
-export default async function Page() {
-  const user = await currentUser();
+export default function Page() {
+  // const clerkUser = await currentUser();
+
+  const { user: clerkUser } = useUser();
+
+  const clerkUserId = clerkUser?.id;
+
+  useEffect(() => {
+    if (clerkUserId) {
+      const createUser = async () => {
+        const res = await createUserAction(clerkUserId);
+        console.log("res: ", res);
+      };
+
+      createUser();
+    }
+  }, [clerkUserId]);
 
   return (
     <div className='mt-20 text-center'>
-      <p className='text-2xl font-medium'>Main Page / {user?.username}</p>
+      {/* <p className='text-2xl font-medium'>Main Page / {clerkUser?.username}</p> */}
       <Dashboard />
     </div>
   );
