717f27bbd382c354565996b1dff4307e06474dac
Niklas Bialk <niklas.bialk@geberit.com>
2024-02-13 21:02:12 +0100

    💄 optimize ui

--- src/components/dashboard/noorganisation/SearchOrganisation.tsx
+++ src/components/dashboard/noorganisation/SearchOrganisation.tsx
@@ -17,24 +17,30 @@ const SearchOrganisation = () => {
     null,
   );
 
-  const handleSearch = (values: { searchTerm: string }) => {
+  const handleOrganisationSearch = (values: { searchTerm: string }) => {
     toast.promise(searchOrganisation(values.searchTerm), {
       loading: "Organisation wird gesucht...",
       success: (data) => {
         setSearchResults(data);
+        if (data.length === 0)
+          throw new Error("Keine Organisationen gefunden.");
         return "Organisationen gefunden";
       },
-      error: "Fehler beim Suchen der Organisation",
+      error: (err) => {
+        return err.message;
+      },
     });
   };
 
   return (
     <div>
-      <p className='mb-2 mt-4 text-2xl font-medium'>Organisation Suchen</p>
+      <p className='mb-2 mt-4 text-center text-2xl font-medium'>
+        Organisation Suchen
+      </p>
       <Formik
         validationSchema={SearchOrganisationSchema}
         initialValues={{ searchTerm: "" }}
-        onSubmit={handleSearch}
+        onSubmit={handleOrganisationSearch}
       >
         {({ errors }) => (
           <Form>
@@ -47,26 +53,31 @@ const SearchOrganisation = () => {
                 error={errors.searchTerm}
               />
             </div>
-            <Button type='submit' variant='outline' className='mt-3'>
+            <Button
+              type='submit'
+              variant='outline'
+              className='mx-auto mt-3 flex w-1/3'
+            >
               Suchen
             </Button>
           </Form>
         )}
       </Formik>
-      <div className="mt-4">
+      <div className='mt-4'>
         {searchResults && searchResults.length > 0 && (
-          <div className="p-4 bg-gray-100 w-1/3 mx-auto">
-            <div className="space-y-2">
-              {searchResults.map((organisation: Organisation) => (
-                <div
-                  key={organisation.id}
-                  className="flex items-center justify-between p-2 bg-white shadow rounded"
-                >
-                  <p className="text-xl font-semibold">{organisation.name}</p>
-                  <Button variant="outline">Anfrage</Button>
-                </div>
-              ))}
-            </div>
+          <div className='mx-auto w-1/3 space-y-2'>
+            <p className='font-medium'>Ergebnisse:</p>
+            {searchResults.map((organisation: Organisation) => (
+              <div
+                key={organisation.id}
+                className='flex items-center justify-between rounded bg-white px-3 py-2'
+              >
+                <p className='text-lg font-semibold'>{organisation.name}</p>
+                <Button onClick={() => console.log("Request...")} size={"sm"}>
+                  Anfragen
+                </Button>
+              </div>
+            ))}
           </div>
         )}
       </div>
