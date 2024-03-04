681154741a5f6534099166be62509b1c5550eb6a
Leunam129 <man.bauer@aol.com>
2024-03-04 22:05:41 +0100

    Impressum editet and link works

---
+++ src/app/impressum/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+
+import "../globals.css";
+
+import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: "ClubVerse",
+  description: "ClubVerse - Dein Verein im Überblick",
+};
+export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: ReactNode;
+  }>) {
+    return <div className='container mx-auto mt-16'>{children}</div>;
+  }
+  
--- src/mocks/impressum.ts
+++ src/mocks/impressum.ts
@@ -1,8 +1,7 @@
 export const impressumData = {
-  name: "Beispiel GmbH",
+  name: "Clubverse",
   address: "Beispielstraße 123\n12345 Beispielstadt\nDeutschland",
   contact: "E-Mail: info@beispiel.de\nTelefon: +49 (0) 123 456789",
-  vatId: "DE123456789",
-  register: "Amtsgericht Beispielstadt",
-  registerNumber: "HRB 12345",
+  vatId: "Nach § 19 Abs.1 UstG wird keine Umsatzsteuer berechnet.",
+  information: "Nach sorgfältiger Inhaltlicher Kontrolle wird keine Haftung für die Inhalte externer Links übernommen. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.",
 };
