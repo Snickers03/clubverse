7ce64b502b6ebc1cb53ded4dd0bbfe94738b1d91
Niklas Bialk <niklas.bialk@geberit.com>
2024-03-14 01:08:21 +0100

    add donation ui

--- src/components/main/DonationConfigurator.tsx
+++ src/components/main/DonationConfigurator.tsx
@@ -2,15 +2,23 @@
 
 import { useOrganisationStore } from "@/store/organisation-store";
 
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+
 const DonationConfigurator = () => {
   const organisation = useOrganisationStore((state) => state.organisation);
   return (
     <div>
-      <p className='w-1/2 pt-2 text-gray-600'>
+      <div className='flex items-center space-x-2'>
+        <Switch id='donation-mode' />
+        <Label htmlFor='airplane-mode'>Aktivieren</Label>
+      </div>
+      <p className='w-1/3 pt-2 text-sm text-gray-600'>
         Um eine individuelle Spendenseite zu erstellen musst du sie hier
-        individal erstellen und live schalten. Die Seite wird unter{" "}
+        individal erstellen und live schalten. <br />
+        Die Seite wird unter{" "}
         <span className='font-bold'>
-          https://clubverse.org/{organisation?.name.replace(" ", "-")}
+          https://clubverse.org/{organisation?.name.replaceAll(" ", "-")}
         </span>{" "}
         erreichbar sein
       </p>
